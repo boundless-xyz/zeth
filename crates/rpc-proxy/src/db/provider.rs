2a9fda33888002dc9f6f9edbba8cd4532529c08b
Wolfgang Welz <welzwo@gmail.com>
2026-01-30 16:49:15 +0100

    chore: Add workspace lints and standardize package metadata (#201)
    * Add workspace lints and standardize package metadata
    
    * update license header

--- crates/rpc-proxy/src/db/provider.rs
+++ crates/rpc-proxy/src/db/provider.rs
@@ -31,7 +31,7 @@ use tracing::trace;
 
 /// Errors returned by the [ProviderDb].
 #[derive(Debug, thiserror::Error)]
-pub enum Error {
+pub(crate) enum Error {
     #[error("{0} failed")]
     Rpc(&'static str, #[source] TransportError),
     #[error("block not found")]
@@ -50,7 +50,7 @@ impl DBErrorMarker for Error {}
 /// immediate context is only synchronous, but a transitive caller is async, use
 /// [tokio::task::spawn_blocking] around the calls that need to be blocked.
 #[derive(Clone)]
-pub struct ProviderDb<N: Network, P: Provider<N>> {
+pub(crate) struct ProviderDb<N: Network, P: Provider<N>> {
     /// Provider to fetch the data from.
     provider: P,
     /// Configuration of the provider.
